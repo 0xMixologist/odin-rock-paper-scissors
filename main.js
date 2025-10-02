10233fc809e3ddb1d0ccec367b4c80ea7ce6f176
0xMixologist <daniel.ctpr@gmail.com>
2025-10-02 17:19:55 +0100

    Create getComputerChoice function
    Creates the function that will generate a random move to be played
    against the user

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("test");
+function getComputerChoice() {
+  let value = Math.random();
+  let compMove;
+  if (value < 0.33) {
+    compMove = "Rock"
+  } else if (value >= 0.33 && value < 0.66) {
+    compMove = "Paper"
+  } else {
+    compMove = "Scissors"
+  }
+  return compMove;
+}
+
+console.log(getComputerChoice());
+    
+
