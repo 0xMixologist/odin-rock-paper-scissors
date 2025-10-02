de53661159b6e870a97c654dce7a3eaeb63b78ee
0xMixologist <daniel.ctpr@gmail.com>
2025-10-02 17:36:27 +0100

    Create the getHumanChoice function
    Adds the function that will read the user's move

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let prompt_message = "Make your move! "
+
 function getComputerChoice() {
   let value = Math.random();
   let compMove;
@@ -13,4 +15,19 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
     
+function getHumanChoice(message) {
+  let input = prompt(message);
+  let fchar = input[0].toUpperCase();
+  let rest = (input.slice(1, input.length)).toLowerCase();
+  let playerMove = fchar + rest;
+  console.log("This is the player move: " + playerMove);
+  if (playerMove != "Rock" && playerMove != "Paper" && playerMove != "Scissors") { 
+    return 1;
+  } else {
+    return playerMove;
+  }
+}
+
+console.log(getHumanChoice(prompt_message));
+
 
